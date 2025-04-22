39284a41283f3ed17f8c498a55da104dfa148550
Huỳnh Trung Tín <httin2901@gmail.com>
2025-04-22 14:09:29 +0700

    first commit

---
+++ BankManager/src/test/java/com/tinhuynhtrung/BankManager/BankManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tinhuynhtrung.BankManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
